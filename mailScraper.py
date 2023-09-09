d204195ff5e335909503aad8e87c121e757b2733
Denis Ronchese <denis.ronchese@gmail.com>
2023-09-09 14:28:19 +0200

    Handling authentication and accessing UNREAD emails

--- mailScraper.py
+++ mailScraper.py
@@ -1,22 +1,63 @@
-import config
-import google.auth
+import os.path
+import json
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# Authenticate with Gmail API
-credentials, _ = google.auth.default()
-service = build('gmail', 'v1', credentials=credentials)
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify']
 
-# List unread emails
-results = service.users().messages().list(userId='me', labelIds=['INBOX', 'UNREAD']).execute()
-messages = results.get('messages', [])
 
-if not messages:
-    print('No unread messages found.')
-else:
-    print('Unread messages:')
-    for message in messages:
+def main():
+
+  creds = authenticate()
+  
+  try:
+    # Call the Gmail API
+    service = build('gmail', 'v1', credentials=creds)
+    # List unread emails
+    results = service.users().messages().list(userId='me', labelIds=['INBOX', 'UNREAD']).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+      print('No unread messages found.')
+    else:
+      print('Unread messages:')
+      for message in messages:
         msg = service.users().messages().get(userId='me', id=message['id']).execute()
-        print(f'Subject: {msg["subject"]}')
+        json_msg = json.dumps(msg, indent=2)
+        print(json_msg)
+        # print(f'Subject: {msg["subject"]}')
         # Process the email content here
 
-# Add further processing logic for email content
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f'An error occurred: {error}')
+
+
+def authenticate():
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES)
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+      token.write(creds.to_json())
+  return creds
+
+
+if __name__ == '__main__':
+  main()
